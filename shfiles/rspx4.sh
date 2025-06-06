57505b9c9794ad6ae305b9d9df7e1206e8df3b53
1OngJ <sthfornil@foxmail.com>
2025-06-06 17:56:23 +0800

    配置完成的VINS-FUSION，跟着文档编译即可使用

--- shfiles/rspx4.sh
+++ shfiles/rspx4.sh
@@ -1,5 +1,8 @@
-sudo chmod 777 /dev/ttyACM0 & sleep 2;
-roslaunch realsense2_camera rs_camera.launch & sleep 10;
-roslaunch mavros px4.launch & sleep 10;
-roslaunch vins fast_drone_250.launch
-wait;
+roslaunch realsense2_camera rs_camera.launch & sleep 5;     
+roslaunch mavros px4.launch & sleep 5;                      
+roslaunch vins fast_drone_250.launch & sleep 3;             
+roslaunch ego_planner single_run_in_exp.launch & sleep 2;   
+rosrun mavros mavcmd long 511 105 5000 0 0 0 0 0            
+rosrun mavros mavcmd long 511 31  5000 0 0 0 0 0            
+roslaunch ego_planner rviz.launch & rqt_image_view                       
+wait;                                                       
